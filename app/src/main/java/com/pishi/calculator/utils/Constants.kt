1c36f550421ba7d39822a659e572b878d711fc1c
PishiKe <nashpeter33@gmail.com>
2021-09-24 05:28:34 +0300

    update

--- app/src/main/java/com/pishi/calculator/utils/Constants.kt
+++ app/src/main/java/com/pishi/calculator/utils/Constants.kt
@@ -1,4 +1,28 @@
 package com.pishi.calculator.utils
 
-class Constants {
+object Constants {
+
+    const val PLUS = "plus"
+    const val MULTIPLY = "multiply"
+    const val MODULUS = "modulus"
+    const val MINUS = "minus"
+    const val DIVIDE = "divide"
+    const val EQUALS = "equals"
+    const val PERCENT = "percent"
+
+
+    fun operationTypes() :ArrayList<String>{
+
+        val list = ArrayList<String>()
+
+        list.add("plus")
+        list.add("multiply")
+        list.add("modulus")
+        list.add("minus")
+        list.add("divide")
+        list.add("equals")
+        list.add("percent")
+
+        return list
+    }
 }
--- app/src/main/java/com/pishi/calculator/view/fragments/MainFragment.kt
+++ app/src/main/java/com/pishi/calculator/view/fragments/MainFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.pishi.calculator.R
 import com.pishi.calculator.databinding.FragmentMainBinding
+import kotlin.properties.Delegates
 
 class MainFragment : Fragment() {
 
     private lateinit var binding : FragmentMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var baseValue = 0.0f
+    private var secondValue = 0.0f
+    private var isPlus = false
+    private var isMinus = false
+    private var isMultiply = false
+    private var isDivide = false
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +27,7 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
 
         binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        buttons()
 
         return binding.root
     }
@@ -30,33 +35,113 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnClear.setOnClickListener {
+    }
 
+    private fun buttons (){
+        binding.btnClear.setOnClickListener {
+            binding.etIo.text.clear()
         }
-        binding.btnMod.setOnClickListener {  }
-        binding.btnPercentage.setOnClickListener{}
+        binding.btnMod.setOnClickListener { }
+        binding.btnPercentage.setOnClickListener {}
         binding.btnNine.setOnClickListener {
+            numberClicked(9)
         }
-        binding.btnEight.setOnClickListener {  }
-        binding.btnSeven.setOnClickListener {  }
-        binding.btnMultiply.setOnClickListener {  }
+        binding.btnEight.setOnClickListener {
+            numberClicked(8)
+        }
+        binding.btnSeven.setOnClickListener {
+            numberClicked(7)
+        }
+        binding.btnMultiply.setOnClickListener {
+        }
         binding.btnFour.setOnClickListener {
             numberClicked(4)
         }
-        binding.btnFive.setOnClickListener {  }
-        binding.btnSix.setOnClickListener {  }
-        binding.btnDivide.setOnClickListener {  }
-        binding.btnOne.setOnClickListener {  }
-        binding.btnTwo.setOnClickListener {  }
-        binding.btnThree.setOnClickListener {  }
-        binding.btnPlus.setOnClickListener {  }
-        binding.btnDoubleZero.setOnClickListener {  }
-        binding.btnZero.setOnClickListener {  }
-        binding.btnEquals.setOnClickListener {  }
+        binding.btnFive.setOnClickListener {
+            numberClicked(5)
+        }
+        binding.btnSix.setOnClickListener {
+            numberClicked(6)
+        }
+        binding.btnDivide.setOnClickListener { }
+        binding.btnOne.setOnClickListener {
+            numberClicked(1)
+        }
+        binding.btnTwo.setOnClickListener {
+            numberClicked(2)
+        }
+        binding.btnThree.setOnClickListener {
+            numberClicked(3)
+        }
+        binding.btnPlus.setOnClickListener {
+            additionClicked()
+        }
+        binding.btnDoubleZero.setOnClickListener { }
+        binding.btnZero.setOnClickListener { }
+        binding.btnEquals.setOnClickListener {
+            equalClicked()
+        }
     }
 
     private fun numberClicked (numberClicked : Int){
         val number = binding.etIo.text.toString() + numberClicked.toString()
         binding.etIo.setText(number)
+    }
+
+    private fun additionClicked (){
+        isPlus = true
+        baseValue = binding.etIo.text.toString().toFloat()
+        binding.etIo.text.clear()
+    }
+
+    private fun multiplyClicked (){
+        isMultiply = true
+        baseValue = binding.etIo.text.toString().toFloat()
+        binding.etIo.text.clear()
+    }
+
+    private fun divideClicked(){
+        isDivide = true
+        baseValue = binding.etIo.text.toString().toFloat()
+        binding.etIo.text.clear()
+    }
+
+    private fun minusClicked(){
+        isMinus = true
+        baseValue = binding.etIo.text.toString().toFloat()
+        binding.etIo.text.clear()
+
+        if (numberClicked())
+    }
+
+    private fun addition (){
+        secondValue = binding.etIo.text.toString().toFloat()
+        var result = baseValue + secondValue
+        binding.etIo.setText(result.toString())
+        isPlus = false
+    }
+
+    private fun multiply(){
+        secondValue = binding.etIo.text.toString().toFloat()
+        val result = secondValue * baseValue
+        binding.etIo.setText(result.toString())
+    }
+
+
+
+
+    private fun equalClicked (){
+        when {
+            isPlus -> {
+                addition()
+            }
+            isMultiply ->{
+                multiply()
+
+            } else ->{
+                binding.etIo.text.clear()
+            }
+
+        }
     }
 }
